dc713cf05a32c1a0114946a6b7b6a1cb7fee05c7
Aidan Margo <aidanmargo@gmail.com>
2021-07-10 13:35:31 -0500

    Added a start game button, eventlistener to start the game on button click

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const startbtn = document.getElementById('startbtn')
 const rock = document.getElementById('rock');
 const dino = document.getElementById('dino');
 const score = document.getElementById('score');
@@ -5,29 +6,45 @@ const game = document.getElementById('game');
 
 
 
-document.addEventListener('keydown', function() {
-    if(!dino.classList.contains('jump-animation')){
-       jump(); 
-    }
-});
-
-setInterval(() => {
-    score.innerText++;
-
-    const dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue('top'));
-    const rockLeft = parseInt(window.getComputedStyle(rock).getPropertyValue('left'));
-
-    if (rockLeft < -410) {
-        rock.style.display = 'none';
-    } else {
-        rock.style.display = '';
-    }
-
-if (rockLeft< -360 && rockLeft > -410 && dinoTop > 150){
-    alert('Game Over! You scored ' + (score.innerText - 1) + '! \n\nPlay again?')
-    location.reload();
-
+function jump (){
+    dino.classList.add('jump-animation');
+    setTimeout(() => {
+        dino.classList.remove('jump-animation');
+    }, 500)
 }
 
-}, 50);
+
+startbtn.addEventListener('click', () => {
+
+    rock.classList.add('rock-animation');
+
+    setInterval(() => {
+        score.innerText++;
+
+        const dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue('top'));
+        const rockLeft = parseInt(window.getComputedStyle(rock).getPropertyValue('left'));
+
+        if (rockLeft < -410) {
+            rock.style.display = 'none';
+        } else {
+            rock.style.display = '';
+        }
+
+    if (rockLeft< -360 && rockLeft > -410 && dinoTop > 225){
+        alert('Game Over! You scored ' + (score.innerText - 1) + '! \n\nPlay again?')
+        location.reload();
+
+    }
+
+    }, 50);
+    
+    document.addEventListener('keydown', function() {
+        if(!dino.classList.contains('jump-animation')){
+        jump(); 
+        }
+    });
+})
+
+
+
 
